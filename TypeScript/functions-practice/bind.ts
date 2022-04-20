0b5ce651ac9f5567c97e20999793af50fe65a80b
Sebastian <sebas.beltran.98@gmail.com>
2022-04-20 10:15:11 -0500

    Added example of the use of bind()

---
+++ TypeScript/functions-practice/bind.ts
@@ -0,0 +1,15 @@
+// bind() returns a bound function that, when executed later,
+//will have the correct context ("this") for calling the
+// original function.
+
+function list(...numbers: number[]): number[] {
+    return Array.prototype.slice.call(numbers);
+}
+
+let list1 = list(1, 2, 3);
+
+// Using bind
+let bound_list = list.bind(null, 4, 5, 6); // Copies list() and assigns initial parameters 4, 5, 6
+let list2 = bound_list();
+let list3 = bound_list(1, 2, 3, 4, 5);
+let list4 = bound_list(1, 2, 3, 4, 5);
